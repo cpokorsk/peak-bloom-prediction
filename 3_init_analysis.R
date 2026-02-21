5da26919e08f6fff0c1bf52676b6688453fa3150
Callie Pokorski <callie.pokorski@gmail.com>
2026-02-21 10:38:43 -0500

    Add station-location mapping and date parsing
    Introduce a manual STATION_LOCATION_MAP and join it to climate_data to associate NOAA stations with bloom-history locations. Convert climate DATE and bloom_history$bloom_date to Date objects, save updated bloom_history and climate_data RData files, and clean up the environment (rm + gc).

--- 3_init_analysis.R
+++ 3_init_analysis.R
@@ -21,6 +21,12 @@ CLIMATE_DATA_FILES <- c(
     file.path("data", "JA000047759.csv")
 )
 
+# Manual mapping from NOAA station IDs to bloom-history locations
+STATION_LOCATION_MAP <- tibble(
+    STATION = c("USW00013743", "CA001108395", "USW00014732", "SZ000001940", "JA000047759"),
+    location = c("washingtondc", "vancouver", "newyorkcity", "liestal", "kyoto")
+)
+
 # Load the bloom history data
 bloom_history <- CHERRY_BLOOM_FILES %>%
     lapply(read.csv) %>%
@@ -38,7 +44,21 @@ RELEVANT_CLIMATE_COLUMNS <- c("STATION", "DATE", "NAME", "ELEVATION", NOAA_DAILY
 climate_data <- climate_data %>%
     select(any_of(RELEVANT_CLIMATE_COLUMNS))
 
+# Associate each climate station with the bloom-history location
+climate_data <- climate_data %>%
+    left_join(STATION_LOCATION_MAP, by = "STATION")
+
+# Convert the DATE column to Date format
+climate_data$DATE <- as.Date(climate_data$DATE, format = "%Y-%m-%d")
+
+# Calculate the bloom day of year (DOY) for each bloom history record
+bloom_history$bloom_date <- as.Date(bloom_history$bloom_date, format = "%Y-%m-%d")
 
 # Save the loaded data for use in the next steps
 save(bloom_history, file = file.path("data", "bloom_history.RData"))
 save(climate_data, file = file.path("data", "climate_data.RData"))
+
+
+# Clean up the environment
+rm(list = setdiff(ls(), c("bloom_history", "climate_data")))
+gc()
